0abf2ff86fb698f787d143e3acd607a3a640bee0
lesleyburr <burrl@bc.edu>
2014-01-17 16:41:42 -0600

    added goaway class

--- js/email.js
+++ js/email.js
@@ -1,6 +1,6 @@
 // anti-spam email
 jQuery(document).ready(function(){
-        jQuery('span.email').each(function(i) {
+        jQuery('span.goaway').each(function(i) {
                 var text = jQuery(this).text();
                 var address = text.replace("x", "historybowl");
         jQuery(this).text(address);
